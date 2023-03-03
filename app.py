61f182b10fdde11196491b47c591cc49ea5a5651
chrisaddy <chris.william.addy@gmail.com>
2023-03-03 17:10:40 -0500

    add language radio buttons

--- app.py
+++ app.py
@@ -2,11 +2,20 @@ import gradio as gr
 import wikipedia
 import requests
 from google_images_search import GoogleImagesSearch
+import os
+from scraper_api import ScraperAPIClient
 
 
-wikipedia.set_lang("es")
-wikipedia.set_lang("fr")
-wikipedia.set_lang("zh")
+SCRAPER_API_KEY = os.getenv("SCRAPER_API")
+
+
+def set_lang(lang):
+    wikipedia.set_lang("es")
+    wikipedia.set_lang("fr")
+    wikipedia.set_lang("zh")
+    
+    print("yo")
+    print(lang)
 
 
 def load_results(query):
@@ -17,7 +26,20 @@ def load_results(query):
     return page.content
 
 
+def load_images(query):
+    client = ScraperAPIClient('APIKEY')
+    url = "https://www.google.com/search?lang=zn&tbm=isch&q=%E5%85%8B"
+    result = client.get(url = url).text
+    return result
+
+
 with gr.Blocks() as app:
+    language = gr.Radio(["中文", "English", "Español", "日本語"])
+    print(dir(language))
+    print(language.value)
+
+    language.change(set_lang, language.value)
+    
     with gr.Row():
         wiki = gr.Textbox(label="wiki search")
         url = gr.Textbox(label="url")
@@ -36,6 +58,7 @@ with gr.Blocks() as app:
             definition = gr.Textbox(label="definition")
             definition_output = gr.Textbox(label="definition")
             define = gr.Button("define")
+            # picture = gr.
 
 
     submit.click(load_results, wiki, output)
